7ac93805e069cdfe06fb8e8a1b7e0035dd884104
anant-bahuguna <anantbahugunaddn@gmail.com>
2020-05-23 13:15:35 +0530

    add github link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,15 @@ const Footer = () => {
     return (
         <footer className="footr-wrapper" style={{ marginBottom: 10 }}>
             <div className="content has-text-centered">
-                <button className="button">
+                <a
+                    href="https://github.com/anant-bahuguna/image-compressor"
+                    className="button"
+                >
                     <span className="icon">
                         <i className="fab fa-github"></i>
                     </span>
                     <span>Open Sourced on GitHub</span>
-                </button>
+                </a>
             </div>
         </footer>
     );
